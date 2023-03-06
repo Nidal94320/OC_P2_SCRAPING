e2b3aa43af3b39cd129eed3d1a35ab579c349490
Nidal Chateur <nidalchateur@gmail.com>
2023-03-06 17:40:29 +0100

    mentor corrections on P2, part4 before

--- src/csv_Loader.py
+++ src/csv_Loader.py
@@ -12,20 +12,23 @@ from .list_Transformer import ET_categories_url
 
 timestamp = "_" + str(datetime.datetime.now())[0:16].replace(" ", "_").replace(":", "h")
 
-""" Load a list into a csv file 
-    csv_Loader(books_data)"""
+
 
 def csv_Loader(books_data,num_cat):
+    """ Load a list into a csv file 
+    csv_Loader(books_data)"""
+
     output_csv = "./data/Cat" +str(num_cat)+"_"+ str(books_data[1][0]) + timestamp + ".csv"
     with open(output_csv, "w",newline='', encoding="utf-8") as f:
         writer = csv.writer(f, delimiter=",")
         for line in books_data:
             writer.writerow(line)
 
-""" Extracts, Transfoms and Loads all books data from one category into csv and .jpeg
-    def ETL_books_data(category_url)"""
 
 def ETL_books_data(category_url,num_cat):
+    """ Extracts, Transfoms and Loads all books data from one category into csv and .jpeg
+    def ETL_books_data(category_url)"""
+
     list_pages=ET_category_pages(category_url)
     books_url=ET_books_url(list_pages)
     books_data=ET_books_data(books_url)
@@ -39,9 +42,11 @@ def ETL_books_data(category_url,num_cat):
         urllib.request.urlretrieve(url_img_book,"./data/img/"+"Cat"+str(num_cat)+"_"+"Book"+str(num_book)+".jpeg")
         
 
-""" Extracts, Transforms and Loads all books into csv by category 
-    ETL()"""
+
 def ETL():
+    """ Extracts, Transforms and Loads all books into csv by category 
+    ETL()"""
+    
     home_page_url="https://books.toscrape.com/"
     num_cat=0
     for category in ET_categories_url(home_page_url):
